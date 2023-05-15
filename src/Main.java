53b056471e3ab7919dabb70ab4a42b8889563ae9
Lotte0906 <lottevankootenniekerk@hotmail.com>
2023-05-15 11:50:29 +0200

    Version 1

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ik ben Lotte!\n");
         System.out.print(" hoi\n!");
+        System.out.print(" hoi\n!");
     }}
