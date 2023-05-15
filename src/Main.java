90630a687ec729a923fd31890aece8e6aaa3fd7e
Lotte0906 <lottevankootenniekerk@hotmail.com>
2023-05-15 12:12:03 +0200

    Version 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
+    /*
+    Een korte samenvatting over mij
+     */
     public static void main(String[] args) {
-        System.out.println("Ik ben Lotte!\n");
-        System.out.print(" hoi\n!");
-        System.out.print(" hoi\n!");
+        System.out.println("Ik ben Lotte!");
+        System.out.print(" Ik ben 25 jaar en ");
+        System.out.print(" wordt 9 juni 26!\n\n"); // Dat is al meer dan een kwart eeuw
+        System.out.println("Ik ben lid van het studentenoorkest cm"); // Collegium Musicum
+        System.out.println("Want zingen is leuk!");
+        System.out.println("En de mensen zijn lief");
     }}
